e8ffa74dba8331873c60b038a950b3ddc89345df
RamRamesh1431 <cherukuramesh40@gmail.com>
2022-11-08 17:19:46 +0530

    first commit

---
+++ src/Component/Student.js
@@ -0,0 +1,17 @@
+import React from "react";
+import AddStudent from "./AddStudent";
+
+const Student = () => {
+  return (
+    <div>
+      <div className="card-body">
+        <h1 style={{ textAlign: "center", fontSize: "50px", color: "white" }}>
+          Student Details
+        </h1>
+        <AddStudent />
+      </div>
+    </div>
+  );
+};
+
+export default Student;
